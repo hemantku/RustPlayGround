4f17a292960c104d6ea96592123607c6bdf8f9cc
hemantku <vgh4uster@gmail.com>
2020-04-07 20:56:40 -0700

    Removed unrequired files

--- hello_world/target/debug/deps/cfg_if-7654643fc8005c80.d
+++
@@ -1,7 +0,0 @@
-/Users/vgh/CLionProjects/RustProject/hello_world/target/debug/deps/cfg_if-7654643fc8005c80.rmeta: /Users/vgh/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/Users/vgh/CLionProjects/RustProject/hello_world/target/debug/deps/libcfg_if-7654643fc8005c80.rlib: /Users/vgh/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/Users/vgh/CLionProjects/RustProject/hello_world/target/debug/deps/cfg_if-7654643fc8005c80.d: /Users/vgh/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/Users/vgh/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/vgh/CLionProjects/RustProject/hello_world/target/debug/hello_world: /Users/vgh/CLionProjects/RustProject/hello_world/src/main.rs
